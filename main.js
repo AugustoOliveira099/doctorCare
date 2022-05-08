70ebc4723b160da1aaaa0bcf18ad2acaffb4ff7b
AugustoOliveira099 <104844662+AugustoOliveira099@users.noreply.github.com>
2022-05-08 00:13:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+  showBackToTopButtonOnScroll()
+  showNavOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (targetLine >= sectionTop && targetLine < sectionTop + sectionHeight) {
+    menuElement.classList.add('active')
+  }
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 467) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`#home, 
+#home Image, 
+#home .stats, 
+#services,
+#services header,
+#services .cards,
+#about,
+#about header,
+#about .content`)
